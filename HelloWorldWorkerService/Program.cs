e4b0bc82501f7fcd309ba48bae24426d4c0652ab
Panda <63072537+giovanny110@users.noreply.github.com>
2023-08-20 07:31:40 -0500

    fix errors

---
+++ HelloWorldWorkerService/Program.cs
@@ -0,0 +1,10 @@
+using HelloWorldWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
